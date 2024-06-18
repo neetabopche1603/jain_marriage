48335442d21a9ccc4048bba6afed9d203e5f289c
Neeta Bopche <neetabopche1603@gmail.com>
2024-06-18 23:38:36 +0530

    component issue fixed

--- resources/views/components/search/table-search-input.blade.php
+++ resources/views/components/search/table-search-input.blade.php
@@ -1,12 +1,36 @@
+@php
+    $type = $type ?? 'text';
+    $name = $name ?? '';
+    $label = $label ?? '';
+    $value = $value ?? '';
+    $disabled = $disabled ?? false;
+    $placeholder = $placeholder ?? 'Search...';
+    $min = $min ?? '';
+    $required = $required ?? false;
+    $id = $id ?? $name;
+    $class = $class ?? '';
+    $action = $action ?? '#';
+    $method = $method ?? null;
+    $btn_type = $btn_type ?? 'submit';
+    $btnClass = $btnClass ?? '';
+    $catVal = $catVal ?? '';
+    $roleName = $roleName ?? '';
+@endphp
+
 <div class="mx-2">
     <form action="{{ $action }}" method="{{ $method }}">
         <div class="input-group">
-            <input type="text" class="form-control {{ $class }}" id="{{ $id }}"
-                name="{{ $name }}" value="{{ $value }}" placeholder="{{ $placeholder }}">
+            <input type="{{ $type }}" class="form-control {{ $class }}" id="{{ $id }}"
+                name="{{ $name }}" value="{{ $value }}" placeholder="{{ $placeholder }}"
+                {{ $disabled ? 'disabled' : '' }} {{ $required ? 'required' : '' }} min="{{ $min }}">
             <input type="hidden" name="{{ $roleName }}" value="{{ $catVal }}">
             <div class="input-group-append">
-                <button type="{{ $btn_type }}" class="btn btn-primary {{ $btnClass }}"><i class="fa fa-search"></i></button>
-                <a href="{{ $action }}" class="btn btn-danger {{ $btnClass }}"><i class="fa fa-refresh" aria-hidden="true"></i></a>
+                <button type="{{ $btn_type }}" class="btn btn-primary {{ $btnClass }}">
+                    <i class="fa fa-search"></i>
+                </button>
+                <a href="{{ $action }}" class="btn btn-danger {{ $btnClass }}">
+                    <i class="fa fa-refresh" aria-hidden="true"></i>
+                </a>
             </div>
         </div>
     </form>
